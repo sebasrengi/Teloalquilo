e8892228ba0542b12cd8c74f46c8bc7e6a579df4
sebasrengi <sebas.rengi@hotmail.com>
2025-12-16 19:19:10 -0500

    test0

--- teloalquilo_front/src/pages/Hero.js
+++ teloalquilo_front/src/pages/Hero.js
@@ -1,30 +1,38 @@
-import {View, StyleSheet, Text, Image} from "react-native";
+import {View, StyleSheet, Text, Image, ImageBackground} from "react-native";
 import tw from 'tailwind-react-native-classnames';
 import  foto  from '../../public/Berserk.jpg';
 
 export const Hero = (props) => {
   return (
-    <View style={tw `w-full h-full  font-sans bg-purple-500`}>
-                
-                <View style={tw ` w-full h-full   `}>
-                    <Image source={foto} style={tw `
-                    w-full h-full`
-                    }/>
-                    <View style={tw `flex float-center items-center justify-center Z-20 `}>
+<View className=" w-full h-full  font-sans bg-purple-500">
+  <View className= "w-full h-full">
+    <ImageBackground source={foto} resizeMode="cover" style={styles.image}>
+          <View className="grid grid-rows h-full   Z-10" >
 
-                      <View style={tw `  `}>
-                        <Text>We are a Landing Page</Text>
-                    </View>
+            <View style={tw ` items-center justify-center `}>
+              {/* corregir el tamaño de las letras, esto debe estar en un stylesheet y no con tailwind */}
+              <Text style={tw ` text-4xl text-red-600 `}>We are a Landing Page</Text>
+            </View>
 
-                    <View style={tw `  `}>
-                      <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sed commodo nibh ante facilisis bibendum.</Text>
-                    </View>
+            <View style={tw ` flex flex-col-10 paddin: 20px 0px 0px 0px  items-center justify-center text-8xl bg-yellow-300 `}>
+              <Text style={tw ` text-xl`}> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed dapibus leo nec ornare diam sed commodo nibh ante facilisis bibendum.</Text>
+            </View>
 
-                    <View style={tw `  `}>
-                    </View>
+            <View style={tw `  `}>
+            </View>
 
-                    </View>
-                </View>
-    </View>
-    );
-  };
+          </View>
+    </ImageBackground>
+
+  </View>
+</View>
+  );
+};
+
+
+  const styles = StyleSheet.create({
+    image: {
+      flex: 1,
+      
+  }
+});
